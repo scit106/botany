3ab3e59ee01f528e9642e7ab579704632ef016f5
Michael Bernstein <mikey@cardinalwind.com>
2017-02-13 23:28:05 -0500

    Add environment file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
+require('dotenv').config();
 
 gulp.task('default', function() {
-	console.log('HELLO WORLD');
+	console.log('This is the public key', process.env.TWITTER_PUBLIC_KEY);
+	console.log('This is the secret key', process.env.TWITTER_SECRET_KEY);
 });
